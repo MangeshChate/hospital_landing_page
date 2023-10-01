e662b0efda274cd1e42cfdff372dafdba9d73079
mangesh <mbchate08@gmail.com>
2023-10-01 15:27:55 +0530

    wrefr

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className='hero vh-100 p-lg-3 p-sm-0 '>
       <div className="row">
         <div className="col-lg-6 col-md-6 col-sm-12 p-lg-3 p-sm-0 d-flex justify-content-center flex-column align-content-center">
-          <img src="https://png.pngtree.com/png-clipart/20211116/original/pngtree-girl-doctor-calendar-concept-business-illustration-png-image_6937265.png" alt="" width={350} className='mob-img img-fluid ms-3 d-lg-none d-sm-block'/>
+          <img src="https://cdni.iconscout.com/illustration/premium/thumb/doctor-appointment-7301315-5979639.png" alt="" width={450} className='mob-img img-fluid ms-3 d-lg-none d-sm-block'/>
           <h1 className='w-100 fw-bold  m-lg-5 m-sm-5 p-3 heroTitle ' >
             Smart <span className='text-primary'>AI</span> <br className='br' />scheduling <br /> <span className='text-primary'>ahead</span>
           <p className='fs-4 mt-4 w-lg-75 fw-bold text-muted' style={{fontFamily:"'Indie Flower', cursive"}}>
